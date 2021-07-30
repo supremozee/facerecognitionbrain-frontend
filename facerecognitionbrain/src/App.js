a06c34e7ebce7a80767a51dd4b0328ac94be98f9
unknown <you@example.com>
2021-07-30 18:31:38 +0100

    my new

---
+++ facerecognitionbrain/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component} from 'react'
+import Navigation from './Component/Navigation/Navigation'
+import Logo from './Component/Logo/Logo'
+import ImageLink from './Component/ImageLink/ImageLink'
+
+class App extends Component {
+  render () {
+    return(
+      <div>
+          <Navigation/> 
+         {/*
+          <Logo/>
+          <ImageLink/>
+          <ImageRecognition/>
+         */}
+        </div>
+        )
+      }
+    
+  }
+ 
+
+
+export default App;
---
+++ facerecognitionbrain/src/Component/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navigation = ()=> {
+    return(
+        <nav stye = {{dispay: 'flex', justifyContent: 'flex-end'}} >
+            <p className= 'f3 link dim black underline pointer'> Sign out</p>
+        </nav>
+    )
+}
+
+export default Navigation
